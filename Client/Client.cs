5ad472fb0a877fb85d8cc047b06548d34a7fe301
Aleksandr <evdokimov.aleksandr.2004@gmail.com>
2025-02-23 20:57:00 +0300

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Sockets;
+
+class Client
+{
+    private static string? ip;
+    private static int port = 13000;
+    private static bool SendAndRecive(int number)
+    {
+        try
+        {
+            if (ip is null) throw new Exception("Ip не определён");
+            using TcpClient client = new TcpClient(ip, port);
+            NetworkStream stream = client.GetStream();
+
+            Byte[] data = BitConverter.GetBytes(number);
+            stream.Write(data, 0, 4);
+            
+            Byte[] state = new Byte[1];
+            stream.Read(state, 0, 1);
+            stream.Read(data, 0, 4);
+            int recived = BitConverter.ToInt32(data);
+            Console.WriteLine("Сервер ответил: {0}", recived);
+            return BitConverter.ToBoolean(state);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Исключение: {0}", e.Message);
+            Console.WriteLine("Введите IP заново:");
+            ip = Console.ReadLine();
+            return true;
+        }
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Введите IP:");
+        ip = Console.ReadLine();
+        int number;
+        Console.WriteLine("Введите число для нахождения модуля");
+        bool serverOn = true;
+        while (serverOn)
+        {
+            Console.Write("Введите число: ");
+            try
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                continue;
+            }
+
+            serverOn = SendAndRecive(number);
+        }
+        if (!serverOn) Console.WriteLine("Сервер отключён");
+        Console.ReadLine();
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+class Server
+{
+    private static int port = 13000;
+    private static string? ip;
+    public static void Main()
+    {
+
+        string host = Dns.GetHostName();
+        IPHostEntry ipHost = Dns.GetHostEntry(host);
+        foreach (IPAddress curAdd in ipHost.AddressList)
+        {
+            if (curAdd.AddressFamily.ToString() == ProtocolFamily.InterNetwork.ToString())
+            {
+                ip = curAdd.ToString();
+                Console.WriteLine("Сеть: " + curAdd.AddressFamily.ToString());
+                Console.WriteLine("Адрес: " + ip);
+            }
+        }
+
+        TcpListener? server = null;
+        bool serverActive = true;
+        try
+        {
+            if (ip is null) throw new Exception("IP является null");
+            IPAddress localAddr = IPAddress.Parse(ip);
+            server = new TcpListener(localAddr, port);
+            server.Start();
+
+            Byte[] bytes = new Byte[4];
+
+            while (serverActive)
+            {
+                Console.WriteLine("Ожидаю подключений...");
+                using TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Кто-то подключился");
+
+                NetworkStream stream = client.GetStream();
+
+                stream.Read(bytes, 0, 4);
+                int data = BitConverter.ToInt32(bytes);
+
+                Console.WriteLine("Клиент отправил: {0}", data);
+
+                if (data == 0) serverActive = false;
+
+                data = Math.Abs(data);
+
+                byte[] intBytes = BitConverter.GetBytes(data);
+
+                Byte[] serverState = BitConverter.GetBytes(serverActive);
+                stream.Write(serverState, 0, 1);
+                stream.Write(intBytes, 0, 4);
+
+                Console.WriteLine("Сервер ответил: {0}", data);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Исключение: {0}", e);
+        }
+        finally
+        {
+            if(server is not null) server.Stop();
+        }
+        Console.ReadLine();
+    }
+}
